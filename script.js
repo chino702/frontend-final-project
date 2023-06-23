6bedeac0fcda249475f3bced6f986e9a00c8d9f2
chino702 <chinox3702@gmail.com>
2023-06-22 22:55:21 -0700

    renamed my css file to styles

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
+
     function fetchData() {
+
         fetch('https://www.freeforexapi.com/api/live?pairs=EURUSD,EURGBP,GBPUSD,USDJPY,AUDUSD,USDCHF,NZDUSD,USDCAD,USDZAR')
+
         .then(response => response.json())
+
         .then(data => { 
+
            const rates =data.rates;
+
            const tableBody = document.querySelector('#ratesTable tbody');
+
            for (const pair in rates) {
+
             const row = document.createElement('tr');
             const pairCell = document.createElement('td');
             const rateCell = document.createElement('td');
@@ -20,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function() {
            }
         })
         .catch(error => {
-
+            console.error('Error:', error);
         });
     }  
 
