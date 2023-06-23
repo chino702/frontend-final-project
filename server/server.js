d47eb9a8b081f0a62d3b513ebe5f82de996137e2
chino702 <chinox3702@gmail.com>
2023-06-23 14:58:09 -0700

    added function to add pairs for my page

--- server/server.js
+++ server/server.js
@@ -4,9 +4,7 @@ const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
-app.use(cors({
-  origin: "null"})
-);
+app.use(cors());
 
 app.get('/api/rates', async (req, res) => {
   try {
@@ -20,6 +18,25 @@ app.get('/api/rates', async (req, res) => {
       res.status(200).json(data.rates);
     } else {
       res.status(500).json({ message: 'Failed to fetch exchange rates' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+app.get('/api/rates/:pair', async (req, res) => {
+  try {
+    const pair = req.params.pair;
+    const apiUrl = `https://www.freeforexapi.com/api/live?pairs=${pair}`;
+
+    const response = await axios.get(apiUrl);
+    const data = response.data;
+
+    if (data.code === 200) {
+      res.status(200).json(data.rates[pair]);
+    } else {
+      res.status(500).json({ message: 'Failed to fetch exchange rate' });
     }
   } catch (error) {
     console.error(error);
